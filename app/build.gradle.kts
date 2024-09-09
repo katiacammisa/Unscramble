9b143863e426bedc954f485c2cce5feed3402b94
99kcammisa <katia.cammisa.external@sonova.com>
2024-09-09 19:02:24 -0300

    Data base storage in Room pt1. Add dependencies for Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,10 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit:converter-gson:2.0.0-beta2")
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
